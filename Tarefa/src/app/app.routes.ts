891d6c4b56fc0faf1f4ce44f73d8e02550ff56b9
pedro.souza <pedrosouza.info@mazatarraf.com.br>
2025-05-29 17:29:30 -0300

    feat(app.routes)-Desenvolvimento das rotas

--- Tarefa/src/app/app.routes.ts
+++ Tarefa/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TarefasComponent } from './Pages/tarefas/tarefas.component';
+import { UsuarioComponent } from './Pages/usuario/usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'tarefas', component: TarefasComponent },
+  { path: 'usuario', component: UsuarioComponent },
+  { path: '', redirectTo: 'tarefas', pathMatch: 'full' }, // rota padrão opcional
+  { path: '**', redirectTo: 'tarefas' } // fallback para rotas inexistentes
+];
