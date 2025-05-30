623fd36631b89b138600318ac8b868de6d67e11a
pedro.souza <pedrosouza.info@mazatarraf.com.br>
2025-05-30 17:12:48 -0300

    feat(tarefas.componente)- desenvolvimento dynamic table

--- Tarefa/src/app/Components/tarefas/tarefas.component.ts
+++ Tarefa/src/app/Components/tarefas/tarefas.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PoPageDynamicTableCustomAction, PoPageDynamicTableCustomTableAction, PoPageDynamicTableModule } from '@po-ui/ng-templates';
+import { DynamicTableComponent } from '../../Shared/dynamic-table/dynamic-table.component';
+import { TarefasService } from '../../Services/tarefas.service';
+import { Subscription } from 'rxjs';
+import { PoPageModule, PoToolbarModule } from '@po-ui/ng-components';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-tarefas',
   standalone: true,
-  imports: [],
+  imports: [PoPageDynamicTableModule, DynamicTableComponent, PoPageModule, PoToolbarModule],
   templateUrl: './tarefas.component.html',
   styleUrl: './tarefas.component.css'
 })
-export class TarefasComponent {
+export class TarefasComponent implements OnInit, OnDestroy {
+  
+  public DynamicTableConfig: {
+    actionsRight: boolean,
+    pageCustomActions: PoPageDynamicTableCustomAction[],
+    tableCustomActions: PoPageDynamicTableCustomTableAction[],
+    fieldscolunasbrowse: any[],
+    quickSearchWidth: number,
+    height: number,
+    serviceApi: any
+  } = {
+    actionsRight: true,
+    pageCustomActions: [],
+    tableCustomActions: [],
+    fieldscolunasbrowse: [],
+    quickSearchWidth: 3,
+    height: 300,
+    serviceApi: null
+  };
+
+  tarefasSubscription?: Subscription;
+
+  ngOnInit(): void {}
+
+
+  ngOnDestroy(): void {
+    if(this.tarefasSubscription) {
+      this.tarefasSubscription.unsubscribe();
+    }
+  }
+
+  constructor( private _tarefasService:TarefasService){}
+
+  GetConfigDynamicTable() {
+    this.DynamicTableConfig.actionsRight = true;
+    this.DynamicTableConfig.quickSearchWidth = 3;
+    this.DynamicTableConfig.height = 300; 
+    this.DynamicTableConfig.pageCustomActions =  this._tarefasService.pagaCustomAction();
+    this.DynamicTableConfig.fieldscolunasbrowse = this._tarefasService.fieldscolunasbrowse();
+    this.DynamicTableConfig.serviceApi = this._tarefasService;
+    this.DynamicTableConfig.tableCustomActions = this._tarefasService.tableCustomActions();
+  }
 
 }
