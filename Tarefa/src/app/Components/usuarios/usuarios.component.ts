fea2e8ed012e58d3ed24d62d29380f523e995e5b
PedroHenrique1s <Phsouzahd15@gmail.com>
2025-05-30 19:54:30 -0300

    construction(usuario) -Desenvolvendo tela do usuário reutilzando componente compartilhado

--- Tarefa/src/app/Components/usuarios/usuarios.component.ts
+++ Tarefa/src/app/Components/usuarios/usuarios.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { IdynamicTable } from '../../Interface/idynamic-table';
+import { TarefasService } from '../../Services/tarefas.service';
+import { Subscription } from 'rxjs';
+import { DynamicTableComponent } from '../../Shared/dynamic-table/dynamic-table.component';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [DynamicTableComponent],
   templateUrl: './usuarios.component.html',
-  styleUrl: './usuarios.component.css'
+  styleUrl: './usuarios.component.css',
 })
 export class UsuariosComponent {
+  public DynamicTableConfig: IdynamicTable = {
+    actionsRight: true,
+    pageCustomActions: [],
+    tableCustomActions: [],
+    fieldscolunasbrowse: [],
+    quickSearchWidth: 3,
+    height: 300,
+    serviceApi: null,
+  };
 
+  tarefasSubscription?: Subscription;
+
+  ngOnInit(): void {
+    this.GetConfigDynamicTable();
+  }
+
+  constructor(private _tarefasService: TarefasService) {}
+
+  GetConfigDynamicTable() {
+    this.DynamicTableConfig.actionsRight = true;
+    this.DynamicTableConfig.quickSearchWidth = 3;
+    this.DynamicTableConfig.height = 300;
+    this.DynamicTableConfig.pageCustomActions =
+      this._tarefasService.pagaCustomAction();
+    this.DynamicTableConfig.fieldscolunasbrowse =
+      this._tarefasService.fieldscolunasbrowse();
+    this.DynamicTableConfig.serviceApi = this._tarefasService;
+    this.DynamicTableConfig.tableCustomActions =
+      this._tarefasService.tableCustomActions();
+  }
 }
---
+++ Tarefa/src/app/Services/usuario.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { NOpcEnum } from '../Interface/enum';
+import { PoPageDynamicTableCustomAction, PoPageDynamicTableCustomTableAction } from '@po-ui/ng-templates';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor() { }
+
+  public currentNOpc: NOpcEnum = NOpcEnum.Visualizar;
+  public isDataLoaded: boolean = false;
+  public resetForm: boolean = false;
+
+  pagaCustomAction(): Array<PoPageDynamicTableCustomAction> {
+    return [
+      {
+        label: 'Incluir',
+        action: this.onIncluir.bind(this),
+        icon: 'an an-eye-closed'
+      }
+    ];
+  }
+
+  fieldscolunasbrowse(): Array<any> {
+    return [
+      { property: 'idTarefa', label: 'ID Tarefa', width: '100px', filter: true, gridColumns: 4 },
+      { property: 'status', label: 'Status', width: '150px', options: this.statusOptions ,filter: true, gridColumns: 4 },
+      { property: 'descricao', label: 'Descrição', width: '300px', filter: true, gridColumns: 12 }
+    ];
+  }
+
+  tableCustomActions(): Array<PoPageDynamicTableCustomTableAction>{
+    return [
+      {
+        label: 'Visualizar',
+        action: (row: any) => {
+          this.currentNOpc = NOpcEnum.Visualizar;
+          this.isDataLoaded = true;
+          this.resetForm = false;
+        },
+        icon: 'an an-eye-closed'
+      },
+      {
+        label: 'Alterar',
+        action: (row: any) => {
+          this.currentNOpc = NOpcEnum.Alterar;
+          this.isDataLoaded = true;
+          this.resetForm = false;
+        },
+        icon: 'an an-edit'
+      },
+      {
+        label: 'Deletar',
+        action: (row: any) => {
+          this.currentNOpc = NOpcEnum.Deletar;
+          this.isDataLoaded = true;
+          this.resetForm = false;
+        },
+        icon: 'an an-trash'
+      }
+    ];
+  }
+
+  readonly statusOptions: Array<object> = [
+    { value: 0, label: 'Pendente' },
+    { value: 1, label: 'Concluído' }
+  ];
+
+  private onIncluir(): void {
+    this.currentNOpc = NOpcEnum.Incluir;
+    this.isDataLoaded = true;
+    this.resetForm = true;
+  }
+}
