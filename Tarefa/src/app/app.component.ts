04f0e020e79c42af3a3473847c3812a625b48423
pedro.souza <pedrosouza.info@mazatarraf.com.br>
2025-05-30 09:52:05 -0300

    refactor(app.component)- refatoração do componente app

--- Tarefa/src/app/app.component.ts
+++ Tarefa/src/app/app.component.ts
@@ -1,34 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-
-import {
-  PoMenuItem,
-  PoMenuModule,
-  PoPageModule,
-  PoToolbarModule,
-} from '@po-ui/ng-components';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    PoToolbarModule,
-    PoMenuModule,
-    PoPageModule,
-    HttpClientModule,
-    RouterOutlet
-  ],  
+  imports: [RouterOutlet],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Cadastro de Tarefas', action: () => this.router.navigate(['/tarefas']) },
-    { label: 'Cadastro de Usuário', action: () => this.router.navigate(['/usuario']) },
-  ];
-
-  constructor(private router: Router) {}
-}
+export class AppComponent {}
