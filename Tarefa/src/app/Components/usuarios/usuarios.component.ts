bdeacc918958091c94fe9b36b7d6889b6644160e
PedroHenrique1s <Phsouzahd15@gmail.com>
2025-06-17 18:49:05 -0300

    feat(usuarios)- Desenvolvimento do modal do usuários para inclusão, alterar e exclusão

--- Tarefa/src/app/Components/usuarios/usuarios.component.ts
+++ Tarefa/src/app/Components/usuarios/usuarios.component.ts
@@ -3,19 +3,27 @@ import { IdynamicTable } from '../../Interface/idynamic-table';
 import { TarefasService } from '../../Services/tarefas.service';
 import { Subscription } from 'rxjs';
 import { DynamicTableComponent } from '../../Shared/dynamic-table/dynamic-table.component';
-import { PoModalComponent, PoModalModule } from '@po-ui/ng-components';
+import { PoButtonModule, PoDynamicFormComponent, PoDynamicFormField, PoDynamicModule, PoModalComponent, PoModalModule } from '@po-ui/ng-components';
 import { UsuarioService } from '../../Services/usuario.service';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [DynamicTableComponent, PoModalModule],
+  imports: [
+    DynamicTableComponent,
+    PoModalModule,
+    PoDynamicModule,
+    PoButtonModule,
+  ],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css',
 })
 export class UsuariosComponent {
   @ViewChild(PoModalComponent, { static: true }) poModal!: PoModalComponent;
+  @ViewChild(PoDynamicFormComponent, { static: true })
+  poComponet!: PoDynamicFormComponent;
 
+  public fields: Array<PoDynamicFormField> = [];
   public DynamicTableConfig: IdynamicTable = {
     title: '',
     actionsRight: true,
@@ -36,19 +44,35 @@ export class UsuariosComponent {
   constructor(private _usuarioService: UsuarioService) {}
 
   GetConfigDynamicTable() {
-    this.DynamicTableConfig.title               = 'Usuários';
-    this.DynamicTableConfig.actionsRight        = true;
-    this.DynamicTableConfig.quickSearchWidth    = 3;
-    this.DynamicTableConfig.height              = 300;
-    this.DynamicTableConfig.fieldscolunasbrowse = this._usuarioService.fieldscolunasbrowse();
-    this.DynamicTableConfig.serviceApi          = this._usuarioService;
-    this.DynamicTableConfig.tableCustomActions  = this._usuarioService.tableCustomActions();
-    this.DynamicTableConfig.pageCustomActions   = this._usuarioService.pagaCustomAction(() => {
-      this.onIncluir();
-    });
+    this.DynamicTableConfig.title = 'Usuários';
+    this.DynamicTableConfig.actionsRight = true;
+    this.DynamicTableConfig.quickSearchWidth = 3;
+    this.DynamicTableConfig.height = 300;
+    this.DynamicTableConfig.fieldscolunasbrowse =
+      this._usuarioService.fieldscolunasbrowse();
+    this.DynamicTableConfig.serviceApi = this._usuarioService;
+    this.DynamicTableConfig.tableCustomActions =
+      this._usuarioService.tableCustomActions();
+    this.DynamicTableConfig.pageCustomActions =
+      this._usuarioService.pagaCustomAction(() => {
+        this.onIncluir();
+      });
   }
 
   onIncluir() {
+    this.fields = this._usuarioService.fieldsdynamic();
     this._usuarioService.onIncluir(this.poModal);
+  }
+
+
+  closeModal() {
+    this.poComponet.form.reset();
+    this.poModal.close();
+  }
+
+  confirmTask() {}
+
+  restore() {
+    this.poComponet.form.reset();
   }
 }
--- Tarefa/src/app/Services/usuario.service.ts
+++ Tarefa/src/app/Services/usuario.service.ts
@@ -13,7 +13,9 @@ export class UsuarioService {
   public isDataLoaded: boolean = false;
   public resetForm: boolean = false;
 
-  pagaCustomAction(onIncluirCallback: () => void): Array<PoPageDynamicTableCustomAction> {
+  pagaCustomAction(
+    onIncluirCallback: () => void
+  ): Array<PoPageDynamicTableCustomAction> {
     return [
       {
         label: 'Incluir',
@@ -30,21 +32,42 @@ export class UsuarioService {
         label: 'ID Usuario',
         width: '100px',
         filter: true,
-        gridColumns: 4,
+        gridColumns: 6,
       },
       {
         property: 'email',
         label: 'Email',
         width: '150px',
         filter: true,
-        gridColumns: 4,
+        gridColumns: 6,
       },
       {
         property: 'senha',
         label: 'Senha',
         width: '150px',
-        filter: true,
         gridColumns: 12,
+      },
+    ];
+  }
+
+  fieldsdynamic(): Array<any> {
+    return [
+      {
+        property: 'email',
+        label: 'Email',
+        width: '150px',
+        gridColumns: 6,
+        placeholder: 'Digite sua senha',
+      },
+      {
+        property: 'senha',
+        label: 'Senha',
+        width: '150px',
+        secret: true,
+        pattern: '[a-zA]{5}[Z0-9]{3}',
+        errorMessage: '5 caracteres e 3 números para senha',
+        placeholder: 'Digite sua senha',
+        gridColumns: 6,
       },
     ];
   }
