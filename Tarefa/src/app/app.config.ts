2049b712b7d0739b86b97df2e0c252e2945296a9
PedroHenrique1s <Phsouzahd15@gmail.com>
2025-06-26 22:01:43 -0300

    config(angular.json)- Configuração do angular e do app.config

--- Tarefa/src/app/app.config.ts
+++ Tarefa/src/app/app.config.ts
@@ -3,15 +3,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PoHttpRequestModule } from '@po-ui/ng-components';
+import { AuthInterceptor } from './Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     importProvidersFrom([BrowserAnimationsModule, PoHttpRequestModule]),
   ],
-  
 };
