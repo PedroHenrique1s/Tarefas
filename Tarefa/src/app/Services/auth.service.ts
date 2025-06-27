d91da09821980134381418b8e306e9e34104aeee
PedroHenrique1s <Phsouzahd15@gmail.com>
2025-06-26 22:52:32 -0300

    feat(auth.service)-Desenvolvimento do auth serivce com o logout e correção de variáveis

--- Tarefa/src/app/Services/auth.service.ts
+++ Tarefa/src/app/Services/auth.service.ts
@@ -2,17 +2,18 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { PoNotificationService } from '@po-ui/ng-components';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = environment.apiUrl;
   private currentUserSubject: BehaviorSubject<any>;
   public currentUser: Observable<any>;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router, private _poNotification: PoNotificationService) {
     const storedUser = localStorage.getItem('currentUser');
     this.currentUserSubject = new BehaviorSubject<any>(
       storedUser ? JSON.parse(storedUser) : null
@@ -26,7 +27,6 @@ export class AuthService {
 
   // **Este é o seu novo método de login que fará a requisição HTTP**
   login(email: string, password: string): Observable<any> {
-    this.apiUrl = `${this.apiUrl}/login`;
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json', // Indica que estamos enviando JSON
@@ -38,41 +38,44 @@ export class AuthService {
       senha: password,
     };
 
-    return this.http.post<any>(this.apiUrl, payload, httpOptions).pipe(
-      map((user) => {
-        // Sua API deve retornar um objeto que contenha o token JWT
-        // Adapte 'user.token' para o nome da propriedade que sua API usa para o token
-        if (user && user.token) {
-          // Armazena o objeto completo do usuário (incluindo o token) no localStorage
-          localStorage.setItem('currentUser', JSON.stringify(user));
-          // Atualiza o BehaviorSubject para que todos os componentes inscritos saibam que o usuário logou
-          this.currentUserSubject.next(user);
-        }
-        return user; // Retorna o objeto do usuário (ou apenas o token, dependendo do que sua API retorna)
-      })
-    );
+    return this.http
+      .post<any>(`${environment.apiUrl}/login`, payload, httpOptions)
+      .pipe(
+        map((user) => {
+          // Sua API deve retornar um objeto que contenha o token JWT
+          // Adapte 'user.token' para o nome da propriedade que sua API usa para o token
+          if (user && user.token) {
+            // Armazena o objeto completo do usuário (incluindo o token) no localStorage
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            // Atualiza o BehaviorSubject para que todos os componentes inscritos saibam que o usuário logou
+            this.currentUserSubject.next(user);
+          }
+          return user; // Retorna o objeto do usuário (ou apenas o token, dependendo do que sua API retorna)
+        })
+      );
   }
 
   refreshToken(): Observable<any> {
     const refreshToken = this.currentUserValue?.refreshToken;
-    this.apiUrl = `${this.apiUrl}/refresh-token`;
 
     if (!refreshToken)
       return new Observable((observer) => observer.error('No refresh token'));
 
-    return this.http.post<any>(this.apiUrl, { refreshToken }).pipe(
-      map((newTokens) => {
-        const currentUser = this.currentUserValue;
-        const updatedUser = {
-          ...currentUser,
-          token: newTokens.token,
-          refreshToken: newTokens.refreshToken,
-        };
-        localStorage.setItem('currentUser', JSON.stringify(updatedUser));
-        this.currentUserSubject.next(updatedUser);
-        return updatedUser;
-      })
-    );
+    return this.http
+      .post<any>(`${environment.apiUrl}/refresh-token`, { refreshToken })
+      .pipe(
+        map((newTokens) => {
+          const currentUser = this.currentUserValue;
+          const updatedUser = {
+            ...currentUser,
+            token: newTokens.token,
+            refreshToken: newTokens.refreshToken,
+          };
+          localStorage.setItem('currentUser', JSON.stringify(updatedUser));
+          this.currentUserSubject.next(updatedUser);
+          return updatedUser;
+        })
+      );
   }
 
   // Seu método isAuthenticated foi adaptado para verificar se há um usuário no currentUserSubject
@@ -81,11 +84,33 @@ export class AuthService {
     return this.currentUserSubject.value !== null;
   }
 
-  // Seu método logout, agora para limpar o token do localStorage
   logout(): void {
-    // Remove o item 'currentUser' do localStorage
+    const token = this.getToken();
+
+    if (token) {
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+      this.http.get(`${environment.apiUrl}/logout`, { headers }).subscribe({
+        next: (res: any) => {
+          this._poNotification.warning(res.mensagem);
+          this.finalizarLogout();
+        },
+        error: (err) => {
+          console.warn(
+            'Erro ao fazer logout na API. Forçando logout local.',
+            err
+          );
+          this.finalizarLogout();
+        },
+      });
+    } else {
+      this.finalizarLogout();
+    }
+  }
+
+  private finalizarLogout(): void {
     localStorage.removeItem('currentUser');
-    // Limpa o BehaviorSubject, notificando os componentes que o usuário deslogou
+    this.router.navigate(['/login']);
     this.currentUserSubject.next(null);
   }
 
